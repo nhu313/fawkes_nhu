84729361277857945c68ded606c0b21d8a6b216d
Nhu Nguyen <nhu313@gmail.com>
2018-08-21 23:37:50 -0400

    update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -61,4 +61,12 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+config :fawkes, FawkesWeb.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :fawkes, Fawkes.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 15, # The amount of database connections in the pool
+  ssl: true
