7a498436acc0b051fd4ef1b882828073c36ee6e7
Nhu Nguyen <nhu313@gmail.com>
2018-08-24 14:37:25 -0400

    added shared view

---
+++ lib/fawkes_web/views/shared_view.ex
@@ -0,0 +1,13 @@
+defmodule FawkesWeb.SharedView do
+  use FawkesWeb, :view
+
+  def full_name(people) when is_list(people) do
+    people
+    |> Enum.map(&full_name/1)
+    |> Enum.join(" && ")
+  end
+
+  def full_name(person) do
+    person.first_name <> " " <> person.last_name
+  end
+end
